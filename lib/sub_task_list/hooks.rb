eed79ae430eb5b8a8939288fed201565c5d3cf57
glorydays <glorydays11@gmail.com>
2024-04-12 22:44:36 +0900

    add due_date

--- lib/sub_task_list/hooks.rb
+++ lib/sub_task_list/hooks.rb
@@ -36,14 +36,14 @@ module SubTaskList
         sub_task.tracker_id = issue.tracker_id
         sub_task.project_id = issue.project_id
         sub_task.subject = item["text"]
-        sub_task.due_date = issue.due_date
+        sub_task.due_date = item["due_date"].presence || issue.due_date
         sub_task.category_id = issue.category_id
         sub_task.assigned_to_id = issue.assigned_to_id
         sub_task.priority_id = issue.priority_id
         sub_task.fixed_version_id = issue.fixed_version_id
         sub_task.author_id = issue.author_id
         sub_task.lock_version = issue.lock_version
-        sub_task.start_date = issue.start_date
+        sub_task.start_date = item["start_date"].presence || issue.start_date
         sub_task.parent_id = issue.id
         sub_task.root_id = issue.root_id
         sub_task.is_private = issue.is_private
@@ -58,7 +58,7 @@ module SubTaskList
     def items_params(context={})
       if context[:params][:items].present?
         context[:params].require(:items).map do |item|
-          item.permit(:text, :estimated_hours) if !item[:text].blank?
+          item.permit(:text, :estimated_hours, :start_date, :due_date) if !item[:text].blank?
         end.compact
       else
         {}
