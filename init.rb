325a69f548034bea64ad32407437131c7b6d3647
glorydays <glorydays11@gmail.com>
2024-04-17 21:35:12 +0900

    fix subtask button

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :sub_task_list do
   name 'redmine sub task list plugin'
   # author 'tiohsa'
   description 'This is a sub task list plugin for Redmine'
-  version '0.0.1'
+  version '0.0.2'
   # url 'https://github.com/tiohsa/sub_task_list'
   # author_url 'https://github.com/tiohsa/sub_task_list'
 end
